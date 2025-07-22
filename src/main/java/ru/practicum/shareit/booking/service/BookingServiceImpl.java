b40a49c21ecb11bf522bf2a1aa4b7044fd8eb71f
Kanava812 <knvznr@ya.ru>
2025-07-23 02:12:37 +0300

    ФЗ№15

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -14,7 +14,6 @@ import ru.practicum.shareit.item.ItemRepository;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserRepository;
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
 
 @Slf4j
