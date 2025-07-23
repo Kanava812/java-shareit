b9adee4da339b5926f6c32b4f4ff1c23dd888c46
Kanava812 <knvznr@ya.ru>
2025-07-23 19:02:15 +0300

    ФЗ№15

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,9 +1,12 @@
 package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {ShareItApp.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ShareItTests {
 
 	@Test
