ffcd34862f81f32361e715f1a5725a103ae02f3e
Kanava812 <knvznr@ya.ru>
2025-08-03 01:57:03 +0300

    ФЗ№16

--- gateway/test/java/ru/practicum/shareitgateway/ShareItGatewayTests.java
+++ gateway/test/java/ru/practicum/shareitgateway/ShareItGatewayTests.java
@@ -1,13 +1,14 @@
-package ru.practicum.shareit;
+package java.ru.practicum.shareitgateway;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.practicum.shareit.ShareItGateway;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {ShareItApp.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ShareItTests {
+@SpringBootTest(classes = {ShareItGateway.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ShareItGatewayTests {
 
 	@Test
 	void contextLoads() {
--- server/src/test/java/ru/practicum/shareitserver/ShareItServerTests.java
+++ server/src/test/java/ru/practicum/shareitserver/ShareItServerTests.java
@@ -1,13 +1,14 @@
-package ru.practicum.shareit;
+package ru.practicum.shareitserver;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import ru.practicum.shareit.ShareItServer;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {ShareItApp.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ShareItTests {
+@SpringBootTest(classes = {ShareItServer.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ShareItServerTests {
 
 	@Test
 	void contextLoads() {
