4ba352ce1b56f60fd0f45ddc3eb35c7d988144a4
Kanava812 <knvznr@ya.ru>
2025-08-05 02:27:29 +0300

    ФЗ№16

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CreateItemDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CreateItemDto.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Getter
@@ -19,5 +20,6 @@ public class CreateItemDto {
     @NotNull(message = "Значение доступности предмета обязательно.")
     private Boolean available;
 
+    @Positive
     private Long requestId;
 }
