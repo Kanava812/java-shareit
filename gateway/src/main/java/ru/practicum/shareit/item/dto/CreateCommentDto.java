73aeaabe28e0c8e102a74e21b7093d5e38a3104a
Kanava812 <knvznr@ya.ru>
2025-08-05 02:06:10 +0300

    ФЗ№16

--- gateway/src/main/java/ru/practicum/shareit/item/dto/CreateCommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CreateCommentDto.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.item.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -11,7 +11,7 @@ import lombok.*;
 @AllArgsConstructor
 public class CreateCommentDto {
 
-    @NotBlank(message = "Комментарий не может быть пустым.")
+    @NotNull(message = "Комментарий не может быть пустым.")
     @Size(min = 1, max = 500, message = "Длина комментария должна быть от 1 до 500 символов.")
     private String text;
 }
