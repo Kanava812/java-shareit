cce25c9b41d1ef2188efc4d5c9ec135a41b4c0f6
Kanava812 <knvznr@ya.ru>
2025-08-05 00:45:34 +0300

    ФЗ№16

--- server/src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
@@ -81,7 +81,8 @@ public class ItemRequestServiceImpl implements  ItemRequestService {
         log.info("Получение данных о запросе c ID {} вместе с данными об ответах на него", id);
         if (!userRepository.existsById(userId)) {
             throw new EntityNotFoundException("Пользователь не найден");
-        }ItemRequest request = requestRepository.findById(id)
+        }
+        ItemRequest request = requestRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Запрос не найден."));
         List<ItemForRequestDto> items = itemRepository.findByRequestId(id, Sort.by(Item.Fields.id)).stream()
                 .map(ItemMapper::toItemForRequestDto)
